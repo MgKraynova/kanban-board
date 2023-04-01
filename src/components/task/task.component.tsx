8ba0f065fe35dcb0fdeb9ee8a77ad2ff94252a21
Marina Kraynova <kraynovamg@gmail.com>
2023-04-01 18:27:11 +0300

    add close button to task

--- src/components/task/task.component.tsx
+++ src/components/task/task.component.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import { CloseButton } from '../close-button/close-button.component';
 type TProps = {
   title: string;
 };
@@ -7,6 +8,7 @@ export const Task = ({ title }: TProps) => {
   return (
     <div className="task" data-testid="task">
       <h3 className="taskTitle">{title}</h3>
+      <CloseButton />
     </div>
   );
 };
