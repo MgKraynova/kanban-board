9f079d3ee9ba90398dce6c2c0ed689e0ee2e7b80
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-03-24 17:34:53 +0300

    create app folder and refactor

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Board } from './pages/board/board.component';
+import 'src/app/index.css';
+import { Board } from 'src/pages/board/board.component.tsx';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
