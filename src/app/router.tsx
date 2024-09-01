fc4a7b24bcdf9924dbae9456146c49581bccad70
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-09-01 14:45:35 +0300

    add todosLoader to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Board } from 'pages/board';
+import { Board, todosLoader } from 'pages/board';
 
 export const router = createBrowserRouter([
   {
     path: '/board',
     element: <Board />,
+    loader: todosLoader,
     errorElement: <h1>Произошла неизвестная ошибка</h1>
   }
 ]);
