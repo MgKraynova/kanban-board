11d92e1fa585afb3dd2bcfd64c1f08ba1dc3f41c
mgkraynova <Marina.Kraynova@leroymerlin.ru>
2024-09-01 14:58:18 +0300

    add todos to board component

--- src/pages/board/board.component.tsx
+++ src/pages/board/board.component.tsx
@@ -1,13 +1,17 @@
 import './styles.css';
+import { useLoaderData } from 'react-router-dom';
+import { todosLoader } from 'pages/board';
 import { Section } from 'widgets/section';
 import { AddNewSection } from 'features/add-new-section';
 
 export const Board = () => {
+  const { newTodos, doneTodos, todosInProgress } = useLoaderData() as Awaited<ReturnType<typeof todosLoader>>;
+
   return (
     <div data-testid="board" className="board">
-      <Section title="To-Do" />
-      <Section title="In progress" />
-      <Section title="Done" />
+      <Section todos={newTodos} title="To-Do" />
+      <Section todos={todosInProgress} title="In progress" />
+      <Section todos={doneTodos} title="Done" />
       <AddNewSection />
     </div>
   );
